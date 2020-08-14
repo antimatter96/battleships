0f14573ff6fddc2c20043f6a17e8b397d3a8adeb
Arpit Jain <antimatter96@users.noreply.github.com>
2020-08-15 01:55:02 +0530

    Move more stuff

--- public/src/ship_placement.jsx
+++ public/src/ship_placement.jsx
@@ -10,6 +10,7 @@ class ShipPLacement extends Board {
     let hor = { A: false, B: false, C: false, D: false, E: false };
     let placedBefore = { A: false, B: false, C: false, D: false, E: false };
     let locked = { A: false, B: false, C: false, D: false, E: false };
+    let shipErrors = { A: null, B: null, C: null, D: null, E: null }
 
     this.state = {
       lockReady: false,
@@ -17,6 +18,8 @@ class ShipPLacement extends Board {
       placedBefore: placedBefore,
       hor: hor,
       locked: locked,
+      displayError: null,
+      shipErrors: shipErrors,
     };
   }
 
@@ -39,6 +42,96 @@ class ShipPLacement extends Board {
 
   }
 
+  checkOverlap(valI, valJ, ship, horizontal) {
+    let j = arrOfJ.indexOf(valJ);
+    let i = arrOfI.indexOf(valI);
+    let tempPoints = new Set();
+
+    if (horizontal) {
+      for (let y = j; y < j + lengthOfType[ship]; y++) {
+        tempPoints.add(JSON.stringify({ 'x': i, 'y': y }));
+      }
+    } else {
+      for (let x = i; x < i + lengthOfType[ship]; x++) {
+        tempPoints.add(JSON.stringify({ 'x': x, 'y': j }));
+      }
+    }
+
+    for (let shipType in this.state.pointsOfShip) {
+      if (!Object.prototype.hasOwnProperty.call(this.state.pointsOfShip, shipType)) {
+        continue;
+      }
+      if (shipType != ship) {
+        let intersection = new Set([...this.state.pointsOfShip[shipType]].filter(x => tempPoints.has(x)));
+        if (intersection.size > 0) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  onClickFunction(e) {
+    let ship = e.target.dataset["ship"];
+    if (this.state.locked[ship]) {
+      // classInverter(ship, true);
+      let shipErrors = this.state.shipErrors;
+      shipErrors[ship] = "Locked";
+      this.setState({ shipErrors: shipErrors });
+      return;
+    }
+    // choicesChanged(ship);
+    console.log(e.target);
+  }
+
+  btnRot(e) {
+    let ship = e.target.dataset["ship"];
+    if (this.state.locked[ship]) {
+      // classInverter(ship, true);
+      let shipErrors = this.state.shipErrors;
+      shipErrors[ship] = "Locked";
+      this.setState({ shipErrors: shipErrors });
+      return;
+    }
+    console.log(e.target);
+    let hor = this.state.hor;
+    hor[ship] = !hor[ship]
+
+    this.setState({ hor: hor });
+
+    let dir = hor[ship] ? "Horizontal" : "Vertical";
+    //$('#btnRotIndic' + ship).text("Currently " + dir);
+    //choicesChanged(ship);
+  }
+
+
+  btnDrop(e) {
+    let ship = e.target.dataset["ship"];
+    if (this.state.locked[ship]) {
+      // classInverter(ship, true);
+      let shipErrors = this.state.shipErrors;
+      shipErrors[ship] = "Already Locked";
+      this.setState({ shipErrors: shipErrors });
+      return;
+    }
+
+    if (this.state.placedBefore[ship]) {
+      this.classList.remove('btn-primary');
+      this.classList.add('btn-danger');
+
+      let locked = this.state.locked;
+      locked[ship] = true;
+      this.setState({ locked: locked });
+
+    } else {
+      // classInverter(ship, true);
+      let shipErrors = this.state.shipErrors;
+      shipErrors[ship] = "Please Place before locking";
+      this.setState({ shipErrors: shipErrors });
+    }
+  }
+
   static checkBounds(valI, valJ, ship, horizontal) {
     if (horizontal) {
       if (arrOfJ.indexOf(valJ) + lengthOfType[ship] > 10) {
@@ -59,31 +152,33 @@ class ShipPLacement extends Board {
           <label>Place {`${ship.name}`}</label>
           <br />
           <span className="col-md-1 boardInpt">
-            <input id={`${ship.st}i`} className="form-control inptXY" data-ship={`${ship.st}`} type="number" min="1" max="10" step="1" placeholder="0" />
+            <input id={`${ship.st}i`} className="form-control inptXY" data-ship={`${ship.st}`} type="number" min="1" max="10" step="1" placeholder="0" onChange={this.onClickFunction.bind(this)} />
           </span>
           <span className="col-md-1 boardInpt">
-            <input id={`${ship.st}j`} className="form-control inptXY" data-ship={`${ship.st}`} minLength="1" maxLength="1" placeholder="A" list="defaultNumbers" />
+            <input id={`${ship.st}j`} className="form-control inptXY" data-ship={`${ship.st}`} minLength="1" maxLength="1" placeholder="A" list="defaultNumbers" onChange={this.onClickFunction.bind(this)} />
           </span>
           <span className="col-md-2 boardBtn">
-            <button id={`btnRot${ship.st}`} className="btn btn-info btnRot" data-ship={`${ship.st}`}>Rotate</button>
+            <button id={`btnRot${ship.st}`} className="btn btn-info btnRot" data-ship={`${ship.st}`} onClick={this.btnRot.bind(this)}>Rotate</button>
           </span>
           <span className="col-md-4 boardBtn">
             <button id={`btnRotIndic${ship.st}`} className="btn btn-block btn-default" disabled>Currently Vertical</button>
           </span>
           <span className="col-md-2 col-md-offset-1 boardBtn">
-            <button className="btn btn-primary btn-block btnDrop" data-ship={`${ship.st}`}>Drop</button>
+            <button className="btn btn-primary btn-block btnDrop" data-ship={`${ship.st}`} onClick={this.btnDrop.bind(this)}>Drop</button>
           </span>
           <span className="col-md-12 boardBtn">
-            <label id={`errorPlaceShip${ship.st}`} className="label label-default">.</label>
+            {
+              this.state.shipErrors[ship.st] ? <label className="label label-danger">{this.state.shipErrors[ship.st]}</label> : ""
+            }
           </span>
         </div>
       )
     });
 
     this.displayError = "";
-    if (this.props.displayError) {
+    if (this.state.displayError) {
       this.displayError = (
-        <label id="errorReady">{this.props.displayError}</label>
+        <label id="errorReady">{this.state.displayError}</label>
       );
     }
 
